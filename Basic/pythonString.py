03048efd0dd49758068779e70255d98323cec924
Sandeep Kumar Murmu <90225941+sandeepKumarMurmu@users.noreply.github.com>
2024-10-07 03:33:08 +0000

    ADD : python stirng file added

---
+++ Basic/pythonString.py
@@ -0,0 +1,28 @@
+x1 = 'string tesing'
+
+print("x1 : ",x1)
+
+x2 = "testing check's"
+x3 = "testing quotes 'kkkk'"
+print("x2 : ",x2)
+print("x3 : ",x3)
+print("test x[0] : ",x3[0])
+print("string length : ", len(x3))
+
+print("test" in x3)
+
+if('test' in x3):
+    print("skkk : ksjflksdjfklj")
+
+
+print(x3[2:5])
+
+int1 = 18
+x4 = f"check the age {int1}"
+print("x4 : ",x4)
+print(f"sjfkdsjfk {int1:.2f}")
+print(f"text {2*3}")
+
+x5 = 'testing 123 \'tkskfj\''
+
+print(x5)
