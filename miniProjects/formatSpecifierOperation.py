c785580ebf2586ada1c520be6391e7898be5f68f
Sandeep Kumar Murmu <90225941+sandeepKumarMurmu@users.noreply.github.com>
2024-10-08 07:38:38 +0000

    ADD : string opertaion and format specifier file is added

---
+++ miniProjects/formatSpecifierOperation.py
@@ -0,0 +1,6 @@
+#format specifiers = {:flags} format a value based on what flags are inserted.
+
+price = 2.98394348
+print(f"price is {price}")
+print(f"price is {price:.2f}")
+print(f"price is {price:020}")
---
+++ miniProjects/stringOperation.py
@@ -0,0 +1,17 @@
+# name = input("Enter your name : ")
+
+# print(f"length of my name is {len(name)}")
+# print(f"find the first occurance of e {name.find("e")}")
+# print(f"find the last occurance of e {name.rfind("e")}")
+# print(f"Capatalizing my name {name.capitalize()}")
+# print(f"Convert my name to upper case {name.upper()}")
+# print(f"Convert my name to lower case {name.lower()}")
+# print(f"check if it is digit {name.isdigit()}")
+# print(f"check if it is alphabet {name.isalpha()}")
+
+# print(f"my name is {name}")
+
+credit_card_number = '123-456-789'
+
+print(credit_card_number[0])
+print(credit_card_number[:5])
